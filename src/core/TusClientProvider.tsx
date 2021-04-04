2659d5bb575fc50bab09eb97411ac614995aacb0
kqito <29191111+kqito@users.noreply.github.com>
2021-04-04 18:23:40 +0900

    feat: alert message if the browser is not supported

--- src/core/TusClientProvider.tsx
+++ src/core/TusClientProvider.tsx
@@ -3,9 +3,11 @@ import {
   Dispatch,
   FC,
   useContext,
+  useEffect,
   useMemo,
   useReducer,
 } from 'react';
+import { isSupported } from 'tus-js-client';
 import { TusClientActions } from './tucClientActions';
 
 import {
@@ -14,8 +16,11 @@ import {
   TusClientState,
 } from './tusClientReducer';
 
-const TUS_CLIENT_CONTEXT_HAS_NOT_FOUND_ERROR_MESSAGE =
-  'No TusClient set, use TusClientProvider to set one';
+export const ERROR_MESSAGES = {
+  tusClientHasNotFounded: 'No TusClient set, use TusClientProvider to set one',
+  tusIsNotSupported:
+    'This browser does not support uploads. Please use a modern browser instead.',
+};
 
 const TusClientStateContext = createContext<TusClientState | undefined>(
   undefined
@@ -28,7 +33,7 @@ export const useTusClientState = () => {
   const tusClientState = useContext(TusClientStateContext);
 
   if (!tusClientState) {
-    throw new Error(TUS_CLIENT_CONTEXT_HAS_NOT_FOUND_ERROR_MESSAGE);
+    throw new Error(ERROR_MESSAGES.tusClientHasNotFounded);
   }
 
   return useMemo(() => tusClientState, [tusClientState]);
@@ -38,7 +43,7 @@ export const useTusClientDispatch = () => {
   const tusClientDispatch = useContext(TusClientDispatchContext);
 
   if (!tusClientDispatch) {
-    throw new Error(TUS_CLIENT_CONTEXT_HAS_NOT_FOUND_ERROR_MESSAGE);
+    throw new Error(ERROR_MESSAGES.tusClientHasNotFounded);
   }
 
   return useMemo(() => tusClientDispatch, [tusClientDispatch]);
@@ -49,6 +54,13 @@ export const TusClientProvider: FC = ({ children }) => {
     tusClientReducer,
     tusClientInitialState
   );
+
+  useEffect(() => {
+    if (!isSupported && process.env.NODE_ENV !== 'production') {
+      // eslint-disable-next-line no-console
+      console.error(ERROR_MESSAGES.tusIsNotSupported);
+    }
+  }, []);
 
   return (
     <TusClientStateContext.Provider value={tusClientState}>
